9dabbda69cb9af6c8fd59bd0bc14ebda5f1f4af2
Merle Makoben <merlemakoben@gmail.com>
2022-03-24 21:15:39 +0100

    Set up project with storybook, styled-components and cleaning up

---
+++ src/Globalstyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    font-size: 112.5%;
+  }
+
+  input, label, button, textarea {
+    font-size: 1em;
+  }
+
+`;
